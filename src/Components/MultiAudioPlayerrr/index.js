5ae12ed292511d90bd1f28461466e0e12835d6e4
talhaashah01 <talhaashah01@gmail.com>
2023-06-08 01:24:44 +0500

    Sound playing in Background Check 1

--- src/Components/MultiAudioPlayerrr/index.js
+++ src/Components/MultiAudioPlayerrr/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Dropdown from "react-bootstrap/Dropdown";
@@ -245,6 +245,17 @@ const MultiAudioPlayerrr = () => {
   const handleCloseTimer = () => setShowTimerModal(false);
   const handleShowTimer = () => setShowTimerModal(true);
 
+  const isPlayingRef = useRef(isPlaying);
+  useEffect(() => {
+    isPlayingRef.current = isPlaying;
+  }, [isPlaying]);
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden" && isPlayingRef.current) {
+      handlePauseAll();
+      handlePlayAll();
+    }
+  });
+
   return (
     <>
       <div className={`audioPlayerWrapper ${menuClass}`}>
@@ -383,44 +394,44 @@ const MultiAudioPlayerrr = () => {
             </button>
           </div>
           <div className="customModalContent text-center">
-          <h2 className="modalHeading pageTitle mb-4">Select Time</h2>
-          <div className="timerOptions">
-            <CustomButton
-              variant="secondaryButton fw-normal"
-              text="None"
-              onClick={() => {
-                runTimer(null);
-              }}
-            />
-            <CustomButton
-              variant="secondaryButton fw-normal"
-              text="15 Minutes"
-              onClick={() => {
-                runTimer(15);
-              }}
-            />
-            <CustomButton
-              variant="secondaryButton fw-normal"
-              text="30 Minutes"
-              onClick={() => {
-                runTimer(30);
-              }}
-            />
-            <CustomButton
-              variant="secondaryButton fw-normal"
-              text="1 Hour"
-              onClick={() => {
-                runTimer(60);
-              }}
-            />
-            <CustomButton
-              variant="secondaryButton fw-normal"
-              text="2 Hours"
-              onClick={() => {
-                runTimer(120);
-              }}
-            />
-          </div>
+            <h2 className="modalHeading pageTitle mb-4">Select Time</h2>
+            <div className="timerOptions">
+              <CustomButton
+                variant="secondaryButton fw-normal"
+                text="None"
+                onClick={() => {
+                  runTimer(null);
+                }}
+              />
+              <CustomButton
+                variant="secondaryButton fw-normal"
+                text="15 Minutes"
+                onClick={() => {
+                  runTimer(15);
+                }}
+              />
+              <CustomButton
+                variant="secondaryButton fw-normal"
+                text="30 Minutes"
+                onClick={() => {
+                  runTimer(30);
+                }}
+              />
+              <CustomButton
+                variant="secondaryButton fw-normal"
+                text="1 Hour"
+                onClick={() => {
+                  runTimer(60);
+                }}
+              />
+              <CustomButton
+                variant="secondaryButton fw-normal"
+                text="2 Hours"
+                onClick={() => {
+                  runTimer(120);
+                }}
+              />
+            </div>
           </div>
         </Modal.Body>
       </Modal>
