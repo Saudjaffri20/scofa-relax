f72fc640284c240b878b645a90378012a551b51e
Talha Shah <talhaashah01@gmail.com>
2023-09-19 23:35:03 +0500

    user agent check and redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
+import axios from "axios";
 import WebRoutes from "./Routes";
+import BASEURL from "./Config/global";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import "./Assets/css/style.css";
+import { async } from "q";
 
 function App() {
+  const fetchDevice = async () => {
+    try {
+      const response = await axios.get(`${BASEURL}/api/user/get_device_info`);
+      const device = response.data.data;
+      if (device == "iOS" || device == "Mac OS X") {
+        window.location.href = "https://relax.scofa.com/ios";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDevice();
+  }, []);
+
   return (
     <>
       <WebRoutes />
