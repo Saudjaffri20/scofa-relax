dfc1ced81d993cf74966473b3f587a8d90952f2c
Talha Shah <talhaashah01@gmail.com>
2023-09-22 00:43:31 +0500

    ios device function

--- src/App.js
+++ src/App.js
@@ -10,15 +10,22 @@ import { async } from "q";
 
 function App() {
   const fetchDevice = async () => {
-    try {
-      const response = await axios.get(`${BASEURL}/api/user/get_device_info`);
-      const device = response.data.data;
-      console.log(device)
-      if (device == "iOS" || device == "Mac OS X") {
-        window.location.href = "https://relax.scofa.com/ios";
-      }
-    } catch (error) {
-      console.log(error);
+    // try {
+    //   const response = await axios.get(`${BASEURL}/api/user/get_device_info`);
+    //   const device = response.data.data;
+    //   console.log(device)
+    //   if (device == "iOS" || device == "Mac OS X") {
+    //     window.location.href = "https://relax.scofa.com/ios";
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    const isIOS =
+      /iPad|iPhone|iPod/.test(navigator.platform) ||
+      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
+
+    if (isIOS) {
+      window.location.href = "https://relax.scofa.com/ios";
     }
   };
 
